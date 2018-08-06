bbc88f71f7f720c46fe63448d230e883bc1356d7
rapric2115 <raphael.richardson@gmail.com>
2018-08-06 01:44:35 -0400

    Add files via upload

--- destiniva/src/environments/environment.prod.ts
+++ destiniva/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA6woCI1uSj1xVxWoEWMhzU-D_6Ue2cPi4',
+    authDomain: 'test-cd7df.firebaseapp.com',
+    databaseURL: 'https://test-cd7df.firebaseio.com',
+    projectId: 'test-cd7df',
+    storageBucket: 'test-cd7df.appspot.com',
+    messagingSenderId: '11438554869'
+  }
 };
--- destiniva/src/environments/environment.ts
+++ destiniva/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA6woCI1uSj1xVxWoEWMhzU-D_6Ue2cPi4',
+    authDomain: 'test-cd7df.firebaseapp.com',
+    databaseURL: 'https://test-cd7df.firebaseio.com',
+    projectId: 'test-cd7df',
+    storageBucket: 'test-cd7df.appspot.com',
+    messagingSenderId: '11438554869'
+  }
 };
 
 /*
